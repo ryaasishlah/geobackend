349ea8a6871075e5f2bd0efa9cfa59c09c697031
ryaasishlah <ryaasishlah123@gmail.com>
2024-02-02 08:35:38 +0700

    fix gaes

---
+++ ryaas_test.go
@@ -0,0 +1,71 @@
+package geobackend
+
+import (
+	"fmt"
+	"testing"
+)
+
+var dbname = "gisyas"
+var collname = "post"
+
+func TestGeoIntersects(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", dbname)
+	coordinates := Point{
+		Coordinates: []float64{
+			107.60996400994378, -6.911636154686917,
+		},
+	}
+	datagedung := GeoIntersects(mconn, collname, coordinates)
+	fmt.Println(datagedung)
+}
+
+func TestGeoWithin(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", dbname)
+	coordinates := Polygon{
+		Coordinates: [][][]float64{
+			{
+				{107.60443214379671, -6.905131269636627},
+				{107.60444655183989, -6.905832179706749},
+				{107.60496553082828, -6.905809511262689},
+				{107.60499936141781, -6.905136688887282},
+				{107.60443214379671, -6.905131269636627},
+			},
+		},
+	}
+	datagedung := GeoWithin(mconn, collname, coordinates)
+	fmt.Println(datagedung)
+}
+
+func TestNear(t *testing.T) {
+	mconn := SetConnection2dsphere("MONGOSTRING", "gisyas", "post")
+	coordinates := Point{
+		Coordinates: []float64{
+			107.60858561038731, -6.9135888696341965,
+		},
+	}
+	datagedung := Near(mconn, "post", coordinates)
+	fmt.Println(datagedung)
+}
+
+func TestNearSphere(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "gisyas")
+	coordinates := Point{
+		Coordinates: []float64{
+			107.60473077068144, -6.905467750563176,
+		},
+	}
+	datagedung := NearSphere(mconn, "post", coordinates)
+	fmt.Println(datagedung)
+}
+
+func TestBox(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "gisyas")
+	coordinates := Polyline{
+		Coordinates: [][]float64{
+			{107.6067794338868, -6.914148270960638},
+			{107.60707235969664, -6.914294215762908},
+		},
+	}
+	datagedung := Box(mconn, collname, coordinates)
+	fmt.Println(datagedung)
+}
